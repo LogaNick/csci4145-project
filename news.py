c41c40ab10e285b65365f4ce8b95512eb071f040
Nicholas Barreyre <nicklbarreyre@gmail.com>
2020-02-14 13:33:41 -0400

    changed news module to be at app level

--- news.py
+++ news.py
@@ -1,16 +1,19 @@
 """This file implements the News class."""
+import datetime
 
 class News():
-    def __init__(self, username, body, comments=None):
+    def __init__(self, username, time, body, comments=None):
         """Instantiate a news object
 
         Args:
             username (string): the username of the person posting this news
+            time (datetime.Datetime): the time that the news was inserted in the DB
             body (string): the body of the news -- the news itself
             comments (list of Comment objects, optional): the comments associated with this news object. Defaults to
             None.
         """
         self.username = username
+        self.time = time
         self.body = body
         self.comments = comments #TODO(@nicholasbarreyre) validate this
 
@@ -23,19 +26,22 @@ class News():
     def remove_comment(self, index):
         pass
 
+
     @staticmethod
     def get_news():
         """Returns all news objects from DB."""
         pass
 
 class Comment():
-    def __init__(self, body):
+    def __init__(self, body, time):
         """Instatiate a comment object.
 
         Args:
             body (string): the content of the comment
+            time (datetime.Datetime): the time the comment was made
         """
         self.body = body #TODO(nicholasbarreyre) validate this
+        self.time = time
 
     def edit_body(self, new_body):
         self.body = new_body
